e14e93a97c4e74cc8100601995344281295a7aa3
john.tg <john.tg177@gmail.com>
2024-01-31 17:31:24 +0530

    appointment section

--- src/App.tsx
+++ src/App.tsx
@@ -284,6 +284,126 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* form section */}
+      <section className="px-36 md:h-[677px]">
+        <div className="md:flex gap-4 text-center md:text-left">
+          <div className=" h-24">
+            <p className="uppercase font-bold text-sm text-primary">
+              appointment
+            </p>
+            <h1 className="mt-2 font-semibold text-5xl">
+              Get in touch to book your first appointment
+            </h1>
+            <p className="my-9 leading-6 md:w-[414px]">
+              Far far away, behind the word mountains, far from the countries
+              Vokalia and Consonantia.
+            </p>
+            <ul className="mt-4">
+              <li className="flex gap-4 font-medium text-base mt-2">
+                <Mail className="bg-primary text-white p-1 rounded-full" />
+                <div>(123) 456-7890</div>
+              </li>
+              <li className="flex gap-4 font-medium text-base mt-6">
+                <Phone className="bg-primary text-white p-1 rounded-full" />
+                <div>medico@health.care</div>
+              </li>
+            </ul>
+          </div>
+
+          <form className="bg-white rounded px-8 pt-6 w-[475px]">
+            <div className="mb-4">
+              <input
+                className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone"
+                type="number"
+                placeholder="Phone"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className=" appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                className=" appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Select appointment Type"
+              />
+            </div>
+            <div className="mb-4">
+              <textarea
+                id="message"
+                rows={6}
+                className="block p-2.5 w-full text-sm text-gray-700 border rounded-lg leading-tight focus:outline-none focus:shadow-outline placeholder:text-md"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <div className="mb-4">
+              <button className="btn-primary w-full">Submit Now</button>
+            </div>
+          </form>
+
+          {/* <form className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form> */}
+        </div>
+      </section>
     </div>
   );
 }
